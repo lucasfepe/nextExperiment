28c7dd5a99b69b7e57550a0d384038bb6087bd72
InceptionU Laptop2 <bahram.rostami@gmail.com>
2025-02-27 12:08:02 -0700

    feat: refactor About component structure; replace About.tsx with About/Main.tsx for improved organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero/Main'
-import About from '@/components/About'
+import About from '@/components/About/Main'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import { Section } from '@/components/Section';
