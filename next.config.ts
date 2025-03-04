569da56e8949bfda0a4d156068c449f0ccfe5002
InceptionU Laptop2 <bahram.rostami@gmail.com>
2025-03-04 14:32:24 -0700

    feat: update FlipCard's CardFront to use Next.js Image component for optimized image loading; add remote image pattern in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'opengraph.githubassets.com',
+      },
+    ],
+  },
   /* config options here */
 };
 
--- src/components/FlipCard/components/CardFront.tsx
+++ src/components/FlipCard/components/CardFront.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import styles from '@/components/FlipCard/styles.module.css';
+import Image from 'next/image';
+import React from "react";
+import styles from "@/components/FlipCard/styles.module.css";
 
 interface CardFrontProps {
-    title: string;
-    description: string;
-    thumbnailUrl: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
 }
 
-export const CardFront: React.FC<CardFrontProps> = ({ title, description, thumbnailUrl }) => (
-    <div className={styles.cardFront}>
-        
-        <img src={thumbnailUrl} alt="Project Thumbnail" className={styles.thumbnail} />
-        <div className={styles.contentSmall}>
-            <h5>{title}</h5>
-            
-            <p className={styles.shortDescription}>{description}</p>
-        </div>
-        
-    </div>
+export const CardFront: React.FC<CardFrontProps> = ({
+  title,
+  description,
+  thumbnailUrl,
+}) => (
+  <div className={styles.cardFront}>
+    <Image
+      src={thumbnailUrl}
+      alt="Project Thumbnail"
+      className={styles.thumbnail}
+      width={300} // Adjust this value based on your needs
+      height={150} // Adjust this value based on your needs
+    />
+  </div>
 );
