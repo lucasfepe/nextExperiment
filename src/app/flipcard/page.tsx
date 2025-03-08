b74938176cc04f39c070886948ffed3194c56fa3
Lucas Pereira <lucasferrari4@gmail.com>
2025-03-08 11:54:05 -0700

    trigger vercel

--- src/app/flipcard/page.tsx
+++ src/app/flipcard/page.tsx
@@ -1,11 +1,12 @@
 import { Showcase } from '@/components/Showcase';
-
+const d = 2;
+console.log(d);
 export default function FlipCardPage() {
     return (
         <main>
             <h1>My Projects</h1>
             <Showcase />
         </main>
-        
+
     );
 }
