8c2a126d1f1eb45b7b36fa79bfeb1e3167a910fd
InceptionU Laptop2 <bahram.rostami@gmail.com>
2025-03-11 13:32:34 -0600

    Update email service to send copy to sender

--- src/components/Contact/services/email.ts
+++ src/components/Contact/services/email.ts
@@ -5,6 +5,7 @@ import emailjs from '@emailjs/browser';
 const EMAIL_JS_PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAIL_JS_PUBLIC_KEY;
 const EMAIL_JS_SERVICE_ID = process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE_ID;
 const EMAIL_JS_TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE_ID;
+const EMAIL_JS_TEMPLATE_ID_COPY = process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE_ID_COPY;
 
 import { ContactFormData, EmailServiceParams } from '@/components/Contact/types';
 
@@ -22,7 +23,11 @@ export async function sendEmail(formData: ContactFormData) {
 
     // Initialize with public key
     emailjs.init(EMAIL_JS_PUBLIC_KEY!);
-
+    emailjs.send(
+        EMAIL_JS_SERVICE_ID!,
+        EMAIL_JS_TEMPLATE_ID_COPY!,
+        emailParams
+    );
     return emailjs.send(
         EMAIL_JS_SERVICE_ID!,
         EMAIL_JS_TEMPLATE_ID!,
